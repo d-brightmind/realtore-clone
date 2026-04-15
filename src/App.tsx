c8bb608d7132870efd2ead4f2625d501e254dfbc
d-brightmind <robiuridwanullah123@gmail.com>
2026-04-15 23:01:33 +0100

    creating the header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Profile from "./pages/Profile";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Router>
+        <Header/>
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/sign-in" element={<SignIn />}/>
           <Route path="/sign-up" element={<SignUp />}/>
-          <Route path="/forgot-password" element={<Profile />}/>
-          <Route path="/profile" element={<Offers />}/>
-          <Route path="/offers" element={<ForgotPassword />}/>
+          <Route path="/profile" element={<Profile />}/>
+          <Route path="/offers" element={<Offers />}/>
+          <Route path="/forgot-password" element={<ForgotPassword />}/>
         </Routes>
       </Router>
     </>

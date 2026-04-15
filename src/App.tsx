07dad9c1e022897f1aa09854b3a02168acdeb4bd
d-brightmind <robiuridwanullah123@gmail.com>
2026-04-15 15:51:51 +0100

    Creating pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Home from "./pages/home";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/sign-in" element={<SignIn />}/>
+          <Route path="/sign-up" element={<SignUp />}/>
+          <Route path="/forgot-password" element={<Profile />}/>
+          <Route path="/profile" element={<Offers />}/>
+          <Route path="/offers" element={<ForgotPassword />}/>
+        </Routes>
+      </Router>
     </>
   )
 }

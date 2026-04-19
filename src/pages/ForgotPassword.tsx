7cb7be5dcd93fbd2ef4253abb2277cbab545e49f
d-brightmind <robiuridwanullah123@gmail.com>
2026-04-19 15:37:09 +0100

    Complete Forgot Password page functionality

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router';
 import OAuth from '../components/OAuth';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
+import { toast } from 'react-toastify/unstyled';
+import { useNavigate } from 'react-router';
 
 export default function ForgotPassword() {
+  const navigate = useNavigate();
   const [email, setEmail] = React.useState("");
   function onChange(e: React.ChangeEvent<HTMLInputElement>) {
       setEmail(e.target.value)
     ;
+  }
+  async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Email was sent");
+      navigate("/sign-in");
+    } catch (error) {
+      toast.error("Could not send reset password");
+    }
   }
 
   return (
@@ -17,7 +32,7 @@ export default function ForgotPassword() {
             <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?q=80&w=1073&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Key" className='w-full rounded-2xl'/>
           </div>
           <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-            <form>
+            <form onSubmit={onSubmit}>
               <input className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded transition-ease-in-out duration-200 mb-6' type="email" placeholder='Email address' id='email' value={email} onChange={onChange}/>
               
               <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
@@ -28,7 +43,7 @@ export default function ForgotPassword() {
                   <Link to="/sign-in" className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out'>Sign in instead?</Link>
                 </p>
               </div>
-              <button className='w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800' type='submit'>Send reset password</button>
+              <button className='w-full cursor-pointer bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800' type='submit'>Send reset password</button>
               <div className='flex my-4 before:border-t before:flex-1 items-center before:border-gray-300 after:border-t after:flex-1 after:border-gray-300'>
                 <p className='text-center font-semibold mx-4'>OR</p>
               </div>

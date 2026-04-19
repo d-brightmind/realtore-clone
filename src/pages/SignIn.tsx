9081ad6954fc4663468cef6e1ca07462a6b0bd59
d-brightmind <robiuridwanullah123@gmail.com>
2026-04-19 15:20:57 +0100

    Complete Sign in functionality

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { AiFillEyeInvisible, AiFillEye } from 'react-icons/ai';
 import { Link } from 'react-router';
 import OAuth from '../components/OAuth';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { toast } from 'react-toastify/unstyled';
+import { useNavigate } from 'react-router';
 
 export default function SignIn() {
+  const navigate = useNavigate();
   const [showPassword, setShowPassword] = React.useState(false);
   const [formData, setFormData] = React.useState({
     email: '',
@@ -16,7 +20,19 @@ export default function SignIn() {
       [e.target.id]: e.target.value
     }));
   }
-
+  async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      if (userCredential.user) {
+        navigate("/");
+      }
+      toast.success("Sign-in successful");
+    } catch (error) {
+      toast.error("Could not sign in");
+    }
+  }
   return (
     <section>
       <h1 className='text-3xl text-center mt-6 font-bold'>Sign In</h1>
@@ -25,7 +41,7 @@ export default function SignIn() {
             <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?q=80&w=1073&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Key" className='w-full rounded-2xl'/>
           </div>
           <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-            <form>
+            <form onSubmit={onSubmit}>
               <input className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded transition-ease-in-out duration-200 mb-6' type="email" placeholder='Email address' id='email' value={email} onChange={onChange}/>
               <div className='relative mb-6'>
                 <input className='w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded transition-ease-in-out duration-200 relative' type={showPassword ? "text" : "password"} placeholder='Password' id='password' value={password} onChange={onChange}/>
@@ -41,7 +57,7 @@ export default function SignIn() {
                   <Link to="/forgot-password" className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out'>Forgot password?</Link>
                 </p>
               </div>
-              <button className='w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800' type='submit'>Sign In</button>
+              <button className='w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg cursor-pointer active:bg-blue-800' type='submit'>Sign In</button>
               <div className='flex my-4 before:border-t before:flex-1 items-center before:border-gray-300 after:border-t after:flex-1 after:border-gray-300'>
                 <p className='text-center font-semibold mx-4'>OR</p>
               </div>

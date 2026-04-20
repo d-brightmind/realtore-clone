535160f0e63cc4226d72a9abe6bf1d51988bfbd6
d-brightmind <robiuridwanullah123@gmail.com>
2026-04-20 18:49:47 +0100

    Added oncChange function

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router';
 export default function Profile() {
   const auth = getAuth();
   const navigate = useNavigate();
+  const [changeDetails, setChangeDetails] = React.useState(false);
   const [formData, setFormData] = React.useState({
     name: auth.currentUser?.displayName || 'Ridwan',
     email: auth.currentUser?.email || 'ridwan@example.com'
@@ -18,6 +19,13 @@ export default function Profile() {
     navigate("/sign-in");
   }
 
+  function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value
+    }))
+  }
+
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
@@ -25,19 +33,14 @@ export default function Profile() {
         <div className="w-full md:w-[50%] mt-6 px-3">
           <form action="">
             {/*Name Input*/}
-            <input className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out' disabled type="text" id="name" value={name} onChange={(e) => setFormData((prevState) => ({
-              ...prevState,
-              name: e.target.value
-            }))} />
+            <input className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${changeDetails && "bg-red-200 focus:bg-red-200"
+              }`} onChange={onChange} value={name} id="name" />
             
             {/*Email Input*/}
-            <input className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mt-6' disabled type="email" id="email" value={email} onChange={(e) => setFormData((prevState) => ({
-              ...prevState,
-              email: e.target.value
-            }))} />
+            <input className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mt-6' disabled type="email" id="email" value={email} onChange={onChange} />
 
             <div className='flex justify-between mt-6 mb-6 text-sm sm:text-lg whitespace-nowrap '>
-              <p className='flex items-center  '>Do you want to change your name?<span className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer'>Edit</span></p>
+              <p className='flex items-center  '>Do you want to change your name?<span className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer' onClick={() => setChangeDetails((prevState) => !prevState)}>{changeDetails ? 'Apply Changes' : 'Edit'}</span></p>
               <p className='text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out cursor-pointer' onClick={onLogOut}>Sign out</p>
             </div>
           </form>

006a7fc88653ad76ad878d7d355e92ef31fb4cc8
d-brightmind <robiuridwanullah123@gmail.com>
2026-05-07 23:13:53 +0100

    Hide API keys and other important keys

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,12 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBwmtfoFVkfOELOmD2x5spLltidu3EnfKk",
-  authDomain: "realtore-test.firebaseapp.com",
-  projectId: "realtore-test",
-  storageBucket: "realtore-test.firebasestorage.app",
-  messagingSenderId: "125504009406",
-  appId: "1:125504009406:web:1386fde3f5e4a9b09ddaef"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase

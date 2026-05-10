15950692d6288daec5d5dd3715daed1a95628ddd
d-brightmind <robiuridwanullah123@gmail.com>
2026-05-10 22:21:06 +0100

    Fix some alignments

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -144,7 +144,9 @@ export default function Home() {
         )}
         {rentListings && rentListings.length > 0 && (
           <div className="m-2 mb-6">
-            <h2 className="px-3 text-2xl mt-6 font-semibold">Places for rent</h2>
+            <h2 className="px-3 text-2xl mt-6 font-semibold">
+              Places for rent
+            </h2>
             <Link to="/category/rent">
               <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
                 Show more places for rent

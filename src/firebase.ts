6b6afc3306fcba3785db034cd3cb2dc5ac561516
d-brightmind <robiuridwanullah123@gmail.com>
2026-05-11 21:59:31 +0100

    Removing console log for testing firebase key

--- src/firebase.ts
+++ src/firebase.ts
@@ -19,5 +19,3 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
-console.log("API KEY:", import.meta.env.VITE_FIREBASE_API_KEY);

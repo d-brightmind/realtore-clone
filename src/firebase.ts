0cd55bc1b2846fdc4100d8f24c3214f1c97961f4
d-brightmind <robiuridwanullah123@gmail.com>
2026-05-12 22:02:40 +0100

    Resolved .env file directory issue

--- src/firebase.ts
+++ src/firebase.ts
@@ -19,5 +19,3 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
-console.log("Firebase initialized with config:", import.meta.env.VITE_FIREBASE_API_KEY);
